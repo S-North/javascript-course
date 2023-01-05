eae6f0657bd6ed2eecb9c2edfa09b6af4a274b9c
S-North <stuart.north@gmail.com>
2023-01-05 17:14:47 +0000

    update form lesson with some string/array methods

--- 05-Forms/main.js
+++ 05-Forms/main.js
@@ -2,22 +2,85 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const result = document.getElementById('result')
 const messages = document.getElementById('messages')
+const joined = document.getElementById('joined')
+const reversed = document.getElementById('reversed')
+const totalWords = document.getElementById('totalWords')
 
 input.focus()
 
+const messageArray = []
+
 const handleSubmit = (e) => {
     e.preventDefault()
-    const value = e.target.input.value
 
-    // result.innerHTML = e.target.input.value
-    input.value = ''
-    input.focus()
-
+    const inputValue = e.target.input.value
+    
     // add a new element to the list
     const message = document.createElement('p')
-    message.innerHTML = "felix: " + value
+    message.innerHTML = "felix: " + inputValue
     messages.appendChild(message)
+    
+    messageArray.push(inputValue)
+    console.log(messageArray)
+    
+    // update the yoda text
+    reversed.innerHTML = reverseWords(messageArray[messageArray.length -1])
+    
+    // join elements of arrays together
+    joined.innerHTML = messageArray.join('. ')
+
+    // update the word count
+    totalWords.innerHTML = "Total Words: " + wordCount(messageArray)
+    
+    // reset the input box and focus it
+    input.value = ''
+    input.focus()
 }
 
 form.addEventListener('submit', e => handleSubmit(e))
 
+function reverseWords( string ) {
+    // guard clause
+    // if the array is empty, return an empty string
+    if ( !string || typeof string !== 'string') return ""
+
+    // split the string into an array and reverse it
+    let reversedArray = string.split(' ').reverse()
+
+    // join the array back into a string
+    let reversedString = reversedArray.join(' ')
+
+    // return the reversed string
+    return reversedString
+    
+    // return string.split(' ').reverse().join(' ')
+}
+
+function wordCount ( array ) {
+    // guard clause
+    if (!array || array.length < 0) return 0
+
+    let wordCount = 0
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+        wordCount += array[i].split(' ').length
+    }
+
+    return wordCount
+}
+
+function updateText () {
+    // get the number of elements in an array
+    let arrayLength = messageArray.length
+
+    // access an array element using its index
+    let firstElement = messageArray[0]
+    let secondElement = messageArray[1]
+    let thirdElement = messageArray[2]
+
+    // you can also do this with strings (because strings are actually arrays of characters!)
+    let myString = 'hello world'
+    myString[0] // this equals 'h'
+    myString[6] // this equals 'w'
+    myString[-1] // this equals 'd'
+}
--- whackamole/main.js
+++ whackamole/main.js
@@ -1,33 +1,68 @@
 const board = document.getElementById('board')
+const game = document.getElementById('game')
 const playingarea = board.getBoundingClientRect()
-
-console.log(board.getBoundingClientRect())
+const scoreboard = document.getElementById('score')
 
 let score = 0
 let items = 1
+let speed = 2000
+
+function sleep(interval) {
+    // 
+    return new Promise(resolve => setTimeout(resolve, interval));
+}
 
 function randomNumber (min, max) {
     return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
 function randomLocation () {
-    const left = randomNumber(playingarea.left, playingarea.right)
-    const top = randomNumber(playingarea.top, playingarea.bottom)
+    const left = randomNumber(0, playingarea.width - 32)
+    const top = randomNumber(0, playingarea.height - 32)
 
     return {left, top}
 }
 
-function createMole () {
-    console.log('create mole')
+async function createMole () {
+    console.log(speed)
+    const moleId = items
     const mole = document.createElement('div')
     const location = randomLocation()
-    console.log(location)
 
     mole.setAttribute('id', items)
     mole.className = 'mole'
     mole.style.left = location.left + "px"
-    mole.style.top = location.top + "px"
+    mole.style.top = (location.top) + "px"
+    mole.addEventListener('click', e => {
+        console.log(e.target.id)
+        removeMole(e.target.id)
+        score++
+        scoreboard.innerHTML = score
+    })
+
     board.appendChild(mole)
+
+    // increment the id for the next mole
+    items++
+
+    // manage the speed of the new moles being created
+    await sleep(speed)
+    speed -= 50 // speed up the moles
+
+    if (speed > 0) createMole()
+    // console.log('start timeout')
+    // setTimeout(console.log('finished timeout'), 5000)
+    // moleTimeout(moleId)    
+}
+
+async function removeMole (id) {
+    const mole = document.getElementById(id)
+    console.log(mole)
+    board.removeChild(mole)
+}
+
+async function moleTimeout (moleId) {
+    setTimeout(removeMole(moleId), randomNumber(3000, 5000))
 }
 
 createMole()
